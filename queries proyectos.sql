a2aaaa27b5356dc6a14bb0442c8270f8e3bc4660
Apfol <andresfabi90@gmail.com>
2017-10-10 01:16:18 -0500

    Terminación mostrar productos que no tengan ventas

--- queries proyectos.sql
+++ queries proyectos.sql
@@ -1,11 +1,11 @@
 select categoria from categoria;
 
-select
-    L.nombre as Nombre_Local,  
-    P.nombre as Nombre_Producto, 
+select 
+    P.nombre as Nombre_Producto,
+    L.nombre as Nombre_Local, 
     count(Co.IDProducto) as Ventas_realizadas,
-    max(concat(Cl.nombre, ' ', Cl.apellido)) as Persona_realizo_más_compras,
-    max(Pa.FechaPago) as Fecha_último_pago,
+    if(count(Co.IDProducto) <> 0, max(concat(Cl.nombre, ' ', Cl.apellido)), 'No registra') as Persona_realizo_más_compras,
+    if(count(Co.IDProducto) <> 0, max(Pa.FechaPago), 'No registra') as Fecha_último_pago,
     case
         when max(Pa.FechaPago) > date_add(NOW(), INTERVAL -1 MONTH) then 
             concat('Se realizó hace ', DATEDIFF(CURRENT_DATE, max(Pa.FechaPago)), 
@@ -14,6 +14,7 @@ select
             concat('Se realizó hace ', MONTH(FROM_DAYS(DATEDIFF(CURRENT_DATE, max(Pa.FechaPago)) + 365)), 
                 if(MONTH(FROM_DAYS(DATEDIFF(CURRENT_DATE, max(Pa.FechaPago)) + 365)) > 1, ' meses y ', ' mes y '), 
                 DAY(FROM_DAYS(DATEDIFF(CURRENT_DATE, max(Pa.FechaPago)) + 365)), if(DAY(FROM_DAYS(DATEDIFF(CURRENT_DATE, max(Pa.FechaPago)) + 365)) > 1, ' días', ' día'))
+        when count(Co.IDProducto) = 0 then 'No registra'
         else 
             concat('Se realizó hace ', YEAR(FROM_DAYS(DATEDIFF(CURRENT_DATE, max(Pa.FechaPago)))), 
                 if(YEAR(FROM_DAYS(DATEDIFF(CURRENT_DATE, max(Pa.FechaPago)))) > 1, ' años, ', ' año, '), 
@@ -22,8 +23,8 @@ select
                  DAY(FROM_DAYS(DATEDIFF(CURRENT_DATE, max(Pa.FechaPago)))), if(DAY(FROM_DAYS(DATEDIFF(CURRENT_DATE, max(Pa.FechaPago)))), ' días', ' día'))
         end as Informe_último_pago
     from producto P 
-        inner join compra Co on P.IDProducto = Co.IDProducto
-        inner join local L on L.IDLocal = P.IDLocal
-        inner join pago Pa on Pa.IDPago = Co.IDPago
-        inner join cliente Cl on Cl.IDCliente = Co.IDCliente
-        group by Co.IDProducto;
+        left join compra Co on P.IDProducto = Co.IDProducto
+        left join local L on L.IDLocal = P.IDLocal
+        left join pago Pa on Pa.IDPago = Co.IDPago
+        left join cliente Cl on Cl.IDCliente = Co.IDCliente
+        group by Co.IDProducto, L.nombre, P.nombre;
